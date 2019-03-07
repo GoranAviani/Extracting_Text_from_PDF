1ba97b8dfabb84ceac95f4f5bbd32912452e8549
goranaviani <goranaviani@gmail.com>
2019-03-07 17:14:13 +0100

    reading _pdf file

--- reading_pdf.py
+++ reading_pdf.py
@@ -2,17 +2,16 @@ import PyPDF2
 
 pdfFileObj = open('sample.pdf', 'rb')
 
-
-
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
 
-
+#printing total page numebers of an pdf
 print(pdfReader.numPages)
 
-
+#reading page 1
 pageObj = pdfReader.getPage(0)
 print(pageObj.extractText())
 
+#reading page 2
 pageObj1 = pdfReader.getPage(1)
 print(pageObj1.extractText())
 
