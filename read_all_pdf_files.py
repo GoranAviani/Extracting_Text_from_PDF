452791013d9701e13981f311ae67d10d670b0edc
goranaviani <goranaviani@gmail.com>
2019-03-08 16:37:50 +0100

    Reading several pdf file pdf file extension from list of extensions

--- read_all_pdf_files.py
+++ read_all_pdf_files.py
@@ -1,8 +1,9 @@
 import PyPDF2, os
 
+
 pdfFiles = []
 for filename in os.listdir('.'):
-    if filename.endswith('.pdf'):
+    if filename[-4:] in ['.pdf','.PDF']:
         pdfFiles.append(filename)
 
 
@@ -14,5 +15,5 @@ for filename in pdfFiles:
     pdfFileObj = open(filename, 'rb')
     pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
     pageObj = pdfReader.getPage(0)
-    print("\n here comes: ")
+    print("\n here comes: " + filename)
     print(pageObj.extractText())
